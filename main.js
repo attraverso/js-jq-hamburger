ab3ea90a57b4317fac427ed1ee5cad1ff99da3bd
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-04-29 18:57:43 +0200

    exercise complete

---
+++ main.js
@@ -0,0 +1,9 @@
+/*open menu with 1s transition*/
+$(".fa-bars").click(function() {
+    $('.hamburger-menu').show(1000);
+});
+
+/*close menu with 1s transition*/
+$('.close').click(function(){
+    $('.hamburger-menu').hide(1000);
+});
